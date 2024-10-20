6c2b7cd4bd4b04f0f1680e200a669c2730ba0c1e
Mohit <themohitnair@protonmail.com>
2024-10-20 20:54:07 +0530

    removed unnecessary CORS middleware

--- main.py
+++ main.py
@@ -25,14 +25,6 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
 app.add_middleware(ZwischenMiddleware)
 
 @app.get("/")
